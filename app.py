5703d1ea9d68e5c53a37e629ed6ed5754ed5b3d1
aiaioi <129491245+aiaioi@users.noreply.github.com>
2023-05-31 09:57:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import send_file
 import os
 import shutil
 import subprocess
+import zipfile
 
 app = Flask(__name__)
 
@@ -27,10 +28,11 @@ def add():
 
     format = request.form.get('format')
     format1 = request.form.get('format1')
+    format2 = request.form.get("format2")
 
     if format == "CycloneDX" and format1 == "xml":
-        subprocess.run(["syft submit_file -o cyclonedx-xml | tee output_file/sbom.xml"],shell =True)
-        file_name = "sbom.xml"
+        subprocess.run(["syft submit_file -o cyclonedx-xml | tee output_file/sbom_cyclone.xml"],shell =True)
+        file_name = "sbom_cyclone.xml"
 
 #   if format == "CycloneDX" and format1 == "json":
 #   サブプロセスでLinuxでCycloneDXを実行
@@ -38,21 +40,38 @@ def add():
     
     if format == "CycloneDX" and format1 == "json":
         subprocess.run(["syft submit_file -o cyclonedx-json | tee output_file/sbom.json"],shell =True)
-        file_name = "sbom.json"
-    
-    else:
-        return render_template("result.html")
-#render_template('result.html', result=result)
+        file_name = "sbom_cyclone.json"
 
- #データ処理後削除
-    shutil.rmtree('submit_file')
-    #再度フォルダ作成
-    os.mkdir('submit_file')
-    #解析結果をダウンロードさせる
-    return send_file("./output_file/"+file_name,
-    mimetype="text/plain",
-    as_attachment=True,
-    )
+    if format == "SPDX" and format1 == "json":
+        subprocess.run(["syft submit_file -o spdx-json | tee output_file/sbom.json"],shell =True)
+        file_name = "sbom_spdx.json"
+    
+    if format == "SPDX" and format1 == "xml":
+        return render_template("result.html")
+
+    #脆弱性有無取得する場合
+    if format2 == "Yes":
+    #Grypeで脆弱性を取得
+        subprocess.run(["syft -o json output_file/sbom_cyclone.xml | tee output_file/vulnerability.json"], shell = True)
+    #アーカイブ
+        compFile = zipfile.ZipFile('output_file/sbom.zip', 'w', zipfile.ZIP_STORED)
+        compFile.write('output_file/sbom_cyclone.xml')
+        compFile.write('output_file/vulnerability.json')
+        compFile.close()
+    #圧縮
+        compFile = zipfile.ZipFile('output_file/zippedSbom.zip', 'w', zipfile.ZIP_DEFLATED)
+        compFile.write('output_file/sbom.zip')
+        compFile.close()
+
+    #データ処理後削除
+        shutil.rmtree('submit_file')
+        #再度フォルダ作成
+        os.mkdir('submit_file')
+        #解析結果をダウンロードさせる
+        return send_file("./output_file/zippedSbom.zip",
+        mimetype="text/plain",
+        as_attachment=True,
+        )
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
